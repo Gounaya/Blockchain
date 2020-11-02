88bfcf4ab9cac123fd5afe142139a14e5b95b050
Spirit <dev.gounaya@gmail.com>
2020-11-02 23:21:04 +0100

    function that will return the last element

--- simplified.py
+++ simplified.py
@@ -10,7 +10,10 @@ class Blockchain(object):
         self.pending_transactions = [] #transaction before before the approve will be here.
         self.new_block(previous_hash="The Times 03/Jan/2009.", proof=100) #to add each block to the chain
 
+
+    #Create a new block Json object with informations, after that we reset the pending list and add the block to the chain.
     def new_block(self, proof, previous_hash=None):
+
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
@@ -22,3 +25,9 @@ class Blockchain(object):
         self.chain.append(block)
 
         return block
+
+    # To get the last element add in the chain 
+    @property
+    def last_block(self):
+
+        return self.chain[-1]

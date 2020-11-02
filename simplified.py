bb9a367b9dded99e332bfc1f060789ea245931e1
Spirit <dev.gounaya@gmail.com>
2020-11-02 22:37:24 +0100

    add function to add new block

--- simplified.py
+++ simplified.py
@@ -1,3 +1,24 @@
 import hashlib #for encryption
 import json #to format out blockx
 import time
+
+
+class Blockchain(object):
+
+    def __init__(self, arg):
+        self.chain = [] #chain that we'll add blocks to / my block-chain
+        self.pending_transactions = [] #transaction before before the approve will be here.
+        self.new_block(previous_hash="The Times 03/Jan/2009.", proof=100) #to add each block to the chain
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.pending_transactions,
+            'proof': proof, #
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.pending_transactions = []
+        self.chain.append(block)
+
+        return block
